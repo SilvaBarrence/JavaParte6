c8e3111b9e32f38217d283e62d5a4c672c2a063a
Gabriel <g.dessilva95@gmail.com>
2020-09-13 17:55:29 -0300

    Classes anônimas e Lambdas

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteClassesAnonimas.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteClassesAnonimas.java
@@ -6,11 +6,11 @@ import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
-public class Teste {
+public class TesteClassesAnonimas {
     public static void main(String[] args) {
 
         Conta cc1 = new ContaCorrente(22, 33);
@@ -43,17 +43,31 @@ public class Teste {
         lista.add(cc3);
         lista.add(cc4);
 
-        lista.sort((c1, c2) -> Integer.compare(c1.getNumero(), c2.getNumero()));
+        lista.sort(new Comparator<Conta>() {
 
-        Comparator<Conta> comp = (Conta c1, Conta c2) -> {
+                       @Override
+                       public int compare(Conta c1, Conta c2) {
+                           return Integer.compare(c1.getNumero(), c2.getNumero());
+                       }
+                   }
+        );
 
-            String nomeC1 = c1.getTitular().getNome();
-            String nomeC2 = c2.getTitular().getNome();
-            return nomeC1.compareTo(nomeC2);
+        Comparator<Conta> comp = new Comparator<Conta>() {
+
+            @Override
+            public int compare(Conta c1, Conta c2) {
+
+                String nomeC1 = c1.getTitular().getNome();
+                String nomeC2 = c2.getTitular().getNome();
+                return nomeC1.compareTo(nomeC2);
+
+            }
         };
 
-        lista.sort(comp);
-        lista.forEach((conta) -> System.out.println(conta + ", " + conta.getTitular().getNome()));
+        for (Conta conta :
+                lista) {
+            System.out.println(conta + ", " + conta.getTitular().getNome());
+        }
 
     }
 }
